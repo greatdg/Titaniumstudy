fa31ae7ff1e27ee1a9a8e5523fc41cf2ffc3d674
James Shin <ipooh69@gmail.com>
2014-05-01 19:20:12 +1200

    updated gitignore

--- app/controllers/list.js
+++ app/controllers/list.js
@@ -1,9 +1,16 @@
 var args = arguments[0] || {};
+var people = Alloy.Collections.people;
+
+function transformfunction(item) {
+	var item = item.toJSON();
+	item.title = item.name;
+	return item;
+}
 
 
-var listTableData = [{title: 'James'}, {title: 'Amy'}];
+// var listTableData = [{title: 'James'}, {title: 'Amy'}];
 
-$.listTable.setData(listTableData);	
+// $.listTable.setData(listTableData);	
 	
 $.listTable.addEventListener('click',function(e){
 	var win = Alloy.createController('detail').getView();
