72294b0989d689a255c1c79e9bcd529ba9bc2446
Donggu Lee <webkorea@gmail.com>
2014-05-01 18:31:40 +1200

    UPdated .gitignore

--- Resources/iphone/alloy/styles/list.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-module.exports = [{"isClass":true,"priority":10000.0012,"key":"container","style":{}},{"isClass":true,"priority":10000.0013,"key":"person","style":{top:50,left:0,zIndex:1,}},{"isClass":true,"priority":10000.0014,"key":"head","style":{top:50,left:100,width:120,height:60,zIndex:2,visible:false,}},{"isClass":true,"priority":10000.0015,"key":"cloth","style":{top:190,left:110,width:100,height:120,zIndex:3,}}];
-=======
-module.exports = [{"isClass":true,"priority":10000.0011,"key":"container","style":{}}];
->>>>>>> 1c8b4c774d930b55da82625131ec1ec3c7489025
--- Resources/iphone/alloy/styles/search.js
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-module.exports = [{"isApi":true,"priority":1000.0016,"key":"Window","style":{backgroundColor:"#FFF",}},{"isClass":true,"priority":10000.0017,"key":"container","style":{}}];
-=======
-module.exports = [{"isApi":true,"priority":1000.0012,"key":"Window","style":{backgroundColor:"#FFF",}},{"isClass":true,"priority":10000.0013,"key":"container","style":{}}];
->>>>>>> 1c8b4c774d930b55da82625131ec1ec3c7489025
