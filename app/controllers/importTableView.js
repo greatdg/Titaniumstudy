ba441fbebf93231c099aac21814bae688510b331
Maple Lee <i.am.maple@hotmail.com>
2014-08-21 20:39:40 +1200

    import

--- app/controllers/importTableView.js
+++ app/controllers/importTableView.js
@@ -6,28 +6,21 @@ var fullName = args.fullName || "";
 $.importTableView.checkBtn = false;
 $.importTableView.index = index;
 
-var importCheck = Ti.UI.createButton({
-    backgroundImage: 'untick.png',    
-    //title: "clickme",
-    top: 11,
-    left: 11,
-    width: 22,
-    height: 22,
-    
-});
 
 $.importTableRow.text = fullName;
 
-
-$.importTableRow.addEventListener('click',function(e){
-   if($.importTableRow.checkBtn==true){
+$.importTableView.addEventListener('click',function(e){
+	
+   if($.importTableView.checkBtn==true){
+	
 		$.importTableView.checkBtn = false;
-        importCheck.backgroundImage = 'untick.png';
+        $.importCheck.backgroundImage = 'untick.png';
 			
 	}else{
-        importCheck.backgroundImage = 'tick.png';
+		
+        $.importCheck.backgroundImage = 'tick.png';
 		$.importTableView.checkBtn = true;		
 	}
 });
 
-$.importTableRow.add(importCheck);
+// $.importTableRow.add(importCheck);
